c0b3915c2241fd97343132f1afaa562cad1af3e7
hackerboy09 <isc19350319@gmail.com>
2023-05-06 18:32:55 -0600

    Diseño de pagina inicial

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+function App() {
+
+  return (
+    <div className="container text-center">
+      <div className="row">
+        <div className="col">
+        <h1>QUIZ GAME</h1>
+        <hr />
+
+        <div class="d-grid gap-2 col-6 mx-auto">
+      <button class="btn btn-secondary btn-lg" type="button">
+      <i class="bi bi-pencil-square"> Capturar</i>
+      </button>
+      
+       <button class="btn btn-info btn-lg" type="button">
+       <i class="bi bi-controller"> Jugar</i>
+       </button>
+</div>
+
+        </div>
+      </div>
+      </div>
+  )
+
+}
+
+export default App
